9169cf6b0de84cc7578ff4b489f7a3cbc0019dd7
reck1ess <heeinso@snu.ac.kr>
2020-03-11 23:32:31 +0900

    feat: Toast 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { toast } from "react-toastify";
 
 import Layout from "../components/common/Layout";
 import ContextProvider from "../lib/context";
+import "react-toastify/dist/ReactToastify.min.css";
 
 if (typeof window !== "undefined") {
   require("lazysizes/plugins/attrchange/ls.attrchange.js");
   require("lazysizes/plugins/respimg/ls.respimg.js");
   require("lazysizes");
 }
+
+toast.configure({
+  autoClose: 3000,
+  draggable: false
+});
 
 export default ({ Component, pageProps }) => {
   return (
