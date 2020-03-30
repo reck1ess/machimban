e81709eb7e42a4832ee3074409918b8fec9bcb50
reck1ess <heeinso@snu.ac.kr>
2020-03-31 00:26:57 +0900

    refactor: 가이드 메시지 위치를 _app.js로 이동

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,9 @@ import ContextProvider from "../lib/context";
 import "react-toastify/dist/ReactToastify.min.css";
 import Head from "next/head";
 
+import { FIRST_GUIDE_MESSAGE } from "../lib/utils/constant";
+import notifyInfo from "../lib/utils/notifyInfo";
+
 if (typeof window !== "undefined") {
   require("lazysizes/plugins/attrchange/ls.attrchange.js");
   require("lazysizes/plugins/respimg/ls.respimg.js");
@@ -17,12 +20,15 @@ toast.configure({
 });
 
 export default ({ Component, pageProps }) => {
+  React.useEffect(() => {
+    notifyInfo(FIRST_GUIDE_MESSAGE);
+  }, []);
   return (
     <ContextProvider>
       <Head>
         <script
           type="text/javascript"
-          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.KAKAO_MAP_API_KEY}&libraries=clusterer`}
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.KAKAO_MAP_API_KEY}&libraries=services,clusterer`}
         ></script>
       </Head>
       <Component {...pageProps} />
