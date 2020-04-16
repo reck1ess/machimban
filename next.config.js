a966228d13750f5134af2d4f946a1fd85bd62974
reck1ess <heeinso@snu.ac.kr>
2020-04-16 22:58:06 +0900

    chore: next-offline 설정 변경

--- next.config.js
+++ next.config.js
@@ -5,15 +5,11 @@ const withOffline = require("next-offline");
 const nextConfig = {
   target: "serverless",
   env: {
-    KAKAO_MAP_API_KEY: process.env.KAKAO_MAP_API_KEY
+    KAKAO_MAP_API_KEY: process.env.KAKAO_MAP_API_KEY,
   },
   dontAutoRegisterSw: true,
-  transformManifest: manifest => ["/"].concat(manifest), // add the homepage to the cache
-  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
-  // turn on the SW in dev mode so that we can actually test it
-  generateInDevMode: true,
+  transformManifest: (manifest) => ["/"].concat(manifest),
   workboxOpts: {
-    swDest: "static/service-worker.js",
     runtimeCaching: [
       {
         urlPattern: /\.(?:gif|ico|jpg|jpeg|png|svg|webp)(?:\?|$)/,
@@ -22,9 +18,9 @@ const nextConfig = {
           cacheName: "image-cache",
           expiration: {
             maxEntries: 500,
-            maxAgeSeconds: 60 * 60 * 24 * 7
-          }
-        }
+            maxAgeSeconds: 60 * 60 * 24 * 7,
+          },
+        },
       },
       {
         urlPattern: /apigw/,
@@ -33,13 +29,13 @@ const nextConfig = {
           cacheableResponse: {
             statuses: [0, 200],
             headers: {
-              "x-test": "true"
-            }
-          }
-        }
-      }
-    ]
-  }
+              "x-test": "true",
+            },
+          },
+        },
+      },
+    ],
+  },
 };
 
 module.exports = withOffline(nextConfig);
